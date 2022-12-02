e2c8fa508470e5234088d1b40a497f0482e139ba
flareflo <florianlipp17@gmail.com>
2022-12-02 12:37:43 +0100

    Remove target because im a retard and forgot

--- day_1/day_1/target/debug/deps/day_1-e28f1e321c423993.d
+++
@@ -1,5 +0,0 @@
-/home/flareflo/CLionProjects/advent_of_code_2022/day_1/day_1/target/debug/deps/day_1-e28f1e321c423993: src/main.rs
-
-/home/flareflo/CLionProjects/advent_of_code_2022/day_1/day_1/target/debug/deps/day_1-e28f1e321c423993.d: src/main.rs
-
-src/main.rs:
