3605d7df7a0c4d9262d86f1e27ec89ee8b23940f
FlareFlo <mail@flareflo.dev>
2022-12-04 23:17:18 +0100

    Day 4 solution

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use std::ops::Range;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+	let input = fs::read_to_string("input.txt").expect("Input should exist");
+
+	let pairs: Vec<((Range<usize>, Range<usize>))> = input.split('\n')
+														  .map(|line| line.split(',')
+																		  .map(|ranges| ranges.split('-')
+																							  .map(|pairs| usize::from_str(pairs).expect("Should be unsigned integer"))
+																		  )
+																		  .map(|mut range_parsed| range_parsed.next().unwrap()..range_parsed.next().unwrap())
+														  )
+														  .map(|mut x| (x.next().unwrap(), x.next().unwrap()))
+														  .collect();
+
+	let mut dupes = 0;
+	for (x, y) in &pairs {
+		if is_inside(x.clone(), y.clone()) || is_inside(y.clone(), x.clone()) {
+			dupes += 1;
+		}
+	}
+	println!("{}", dupes);
+
+}
+
+fn is_inside(outer: Range<usize>, inner: Range<usize>) -> bool {
+	outer.start <= inner.start && outer.end >= inner.end
 }
