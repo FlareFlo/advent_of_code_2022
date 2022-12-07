bb6a8574b2203186672f5ba5482b1d547a2d879b
flareflo <florianlipp17@gmail.com>
2022-12-07 13:02:28 +0100

    Day 7 shit dont work

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,74 +1,81 @@
+#![feature(if_let_guard)]
+
 use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::fs::DirEntry;
+use std::str::FromStr;
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash)]
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub struct File {
-    // List of directories from root
-    full_path: Vec<String>,
-
-    size: usize,
+	pub size: usize,
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash)]
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub struct Dir {
-    full_path: String,
-
-    entries: HashSet<Entry>
+	pub entries: Vec<Entry>,
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash)]
+impl Dir {
+	pub fn get_size(&self) -> usize {
+		self.entries.iter().map(|x| x.get_size()).sum()
+	}
+}
+
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub enum Entry {
-    File(File),
-    Directory(Dir),
+	File(File),
+	Directory(Dir),
 }
 
 impl Entry {
-    pub fn get_size(&self) -> usize {
-        match self {
-            Entry::File(file) => {file.size}
-            Entry::Directory(dir) => {
-                dir.entries.iter().map(|x|x.get_size()).sum()
-            }
-        }
-    }
+	pub fn get_size(&self) -> usize {
+		match self {
+			Entry::File(file) => { file.size }
+			Entry::Directory(dir) => {
+				dir.get_size()
+			}
+		}
+	}
 }
 
 fn main() {
-    let file = fs::read_to_string("input.txt").expect("Input should exist");
+	let file = fs::read_to_string("input.txt").expect("Input should exist");
 
-    let mut curr_path: Vec<String> = vec![];
-    let mut root = Entry::Directory(Dir { full_path: "".to_string(), entries: HashSet::new() });
+	let mut at = 0;
+	let entries = eval_line(&file.lines().collect::<Vec<_>>(), &mut at);
 
-    let mut current = None;
-
-    for line in file.lines() {
-        match () {
-            _ if line.starts_with('$') => {
-                let instruction = &line[2..];
-
-                match () {
-                    _ if instruction.starts_with("ls") => {
-                        continue;
-                    }
-                    _ if instruction.starts_with("cd") => {
-                        let split = instruction.split("cd").last().expect("Should contain any values").trim();
-                        match () {
-                            _ if split == ".." => {
-                                curr_path.pop();
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-            _ if line.starts_with("dir") => {
-
-            }
-            _ => {
-
-            }
-        }
-    }
 
 }
+
+fn eval_line(lines: &[&str], at: &mut  usize) -> Vec<Entry> {
+	let mut entries = vec![];
+	loop {
+		let line = &lines[*at];
+		match () {
+			_ if line== &"$ cd .." => {
+				return entries;
+			}
+			// Ignore as this does nothing right now
+			_ if line == &"$ ls" => {}
+			_ if line.starts_with("$ cd") => {
+				entries.append(&mut eval_line(lines, at))
+			}
+			_ if line.starts_with("dir") => {
+				entries.push(Entry::Directory(Dir {
+					entries: vec![],
+				}));
+			}
+			_ if let Ok(size) = usize::from_str(line.split(" ").next().expect("Should have space within it")) => {
+				entries.push(Entry::File(File {
+					size,
+				}))
+			}
+			_ => {
+				panic!("Unclear expression for line {}", line);
+			}
+		}
+		*at += 1;
+	}
+}
+
+
