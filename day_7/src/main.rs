379345ca3632ffad09bfdcf3fa815bb9248e904c
FlareFlo <mail@flareflo.dev>
2022-12-08 00:08:18 +0100

    Day 7 task 1 done, i was increasing the line pointer within the cd function twice

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -6,13 +6,13 @@ use std::fs::DirEntry;
 use std::process::exit;
 use std::str::FromStr;
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Clone)]
 pub struct File {
 	pub name: String,
 	pub size: usize,
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Clone)]
 pub struct Dir {
 	pub name: String,
 	pub entries: Vec<Entry>,
@@ -24,7 +24,7 @@ impl Dir {
 	}
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Clone)]
 pub enum Entry {
 	File(File),
 	Directory(Dir),
@@ -62,16 +62,28 @@ impl Entry {
 	pub fn is_file(&self) -> bool {
 		!self.is_dir()
 	}
-	pub fn file(&mut self) -> Option<&mut File> {
+	pub fn file_mut(&mut self) -> Option<&mut File> {
 		match self {
-			Entry::File(file) => {Some(file)}
-			Entry::Directory(_) => {None}
+			Entry::File(file) => { Some(file) }
+			Entry::Directory(_) => { None }
 		}
 	}
-	pub fn dir(&mut self) -> Option<&mut Dir> {
+	pub fn dir_mut(&mut self) -> Option<&mut Dir> {
 		match self {
-			Entry::File(_) => {None}
-			Entry::Directory(dir) => {Some(dir)}
+			Entry::File(_) => { None }
+			Entry::Directory(dir) => { Some(dir) }
+		}
+	}
+	pub fn file(&self) -> Option<&File> {
+		match self {
+			Entry::File(file) => { Some(file) }
+			Entry::Directory(_) => { None }
+		}
+	}
+	pub fn dir(&self) -> Option<&Dir> {
+		match self {
+			Entry::File(_) => { None }
+			Entry::Directory(dir) => { Some(dir) }
 		}
 	}
 }
@@ -80,14 +92,35 @@ fn main() {
 	let file = fs::read_to_string("input.txt").expect("Input should exist");
 
 	let mut at = 1;
-	let entries = eval_line(&file.lines().collect::<Vec<_>>(), &mut at);
-	dbg!(entries);
+	let slash = Dir {
+		name: "/".to_string(),
+		entries: eval_line(&file.lines().collect::<Vec<_>>(), &mut at),
+	};
 
+	let mut totals = vec![];
+	filter_size(&mut totals, &slash, 100_000);
+
+	println!("{}", totals.iter().map(|x|x.get_size()).sum::<usize>());
 }
 
-fn eval_line(lines: &[&str], at: &mut  usize) -> Vec<Entry> {
+fn filter_size(totals: &mut Vec<Dir>, dir: &Dir, limit: usize) {
+	for entry in &dir.entries {
+		if let Some(dir) = entry.dir() {
+			if dir.get_size() <= limit  {
+				totals.push(dir.clone());
+			}
+			filter_size(totals, dir, limit);
+		}
+	}
+}
+
+fn eval_line(lines: &[&str], at: &mut usize) -> Vec<Entry> {
 	let mut entries = vec![];
 	loop {
+		if lines.len() - 1 == *at {
+			return entries;
+		}
+
 		let line = &lines[*at];
 
 		match () {
@@ -101,14 +134,13 @@ fn eval_line(lines: &[&str], at: &mut  usize) -> Vec<Entry> {
 			_ if line.starts_with("$ cd") => {
 				*at += 1;
 				let dirname = line.split(" ").nth(2).expect("Should have a last element");
-				println!("{}", "-------------------------");
-				dbg!(&entries, dirname);
 				let target_folder = entries.iter_mut()
-										   .filter(|x|x.get_name() == dirname)
+										   .filter(|x| x.get_name() == dirname)
 										   .next().expect("Should always find exactly one directory")
-										   .dir().expect("Has to be a directory");
+										   .dir_mut().expect("Has to be a directory");
 				let mut target_folder_contents = eval_line(lines, at);
 				target_folder.entries.append(&mut target_folder_contents);
+				continue;
 			}
 			_ if line.starts_with("dir") => {
 				entries.push(Entry::Directory(Dir {
